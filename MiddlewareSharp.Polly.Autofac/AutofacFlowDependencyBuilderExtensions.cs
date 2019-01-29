48a7cc4b77e6499059c731e66dfa395aaa5224cc
Sebastian Alex <sebastian.alex@komputronik-biznes.pl>
2019-01-29 16:06:25 +0100

    Fixed missing dependency registration from Polly.

--- MiddlewareSharp.Polly.Autofac/AutofacFlowDependencyBuilderExtensions.cs
+++ MiddlewareSharp.Polly.Autofac/AutofacFlowDependencyBuilderExtensions.cs
@@ -7,8 +7,9 @@ namespace MiddlewareSharp.Polly.Autofac
 	public static class AutofacFlowDependencyBuilderExtensions
 	{
 		public static ContainerBuilder RegisterMiddleware<TContext, TMiddleware>(this ContainerBuilder builder, IAsyncPolicy asyncPolicy)
-			where TMiddleware : IMiddleware<TContext>
+			where TMiddleware : class, IMiddleware<TContext>
 		{
+			builder.RegisterType<TMiddleware>();
 			builder.Register(p => new PollyMiddleware<TContext, TMiddleware>(p.Resolve<TMiddleware>(), asyncPolicy));
 			return builder;
 		}
--- MiddlewareSharp.Polly/PollyFlowDependencyBuilderExtensions.cs
+++ MiddlewareSharp.Polly/PollyFlowDependencyBuilderExtensions.cs
@@ -7,8 +7,9 @@ namespace MiddlewareSharp.Polly
 	public static class PollyFlowDependencyBuilderExtensions
 	{
 		public static IServiceCollection WithMiddleware<TContext, TMiddleware>(this IServiceCollection collection, IAsyncPolicy asyncPolicy)
-			where TMiddleware : IMiddleware<TContext>
+			where TMiddleware : class, IMiddleware<TContext>
 		{
+			collection.AddScoped<TMiddleware>();
 			collection.AddScoped(p => new PollyMiddleware<TContext, TMiddleware>(p.GetService<TMiddleware>(), asyncPolicy));
 			return collection;
 		}
